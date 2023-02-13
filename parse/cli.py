fad4f8a181ce09d16c26d5a8d994ca8d339a397e
dubiZA <d.hall@swordhealth.com>
2023-02-13 13:46:17 -0700

    Add --version (-v) callback function

---
+++ parse/cli.py
@@ -0,0 +1,28 @@
+"""This module provides the parse CLI."""
+# parse/cli.py
+
+from typing import Optional
+
+import typer
+
+from parse import __app_name__, __version__
+
+app = typer.Typer()
+
+def _version_callback(value: bool) -> None:
+    if value:
+        print(f"{__app_name__} v{__version__}")
+
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
