4bc3a76fded0c4fac6e1e5bb8468f97cfabdf228
dubiZA <d.hall@swordhealth.com>
2023-02-17 11:04:09 -0700

    Starting from scratch again

--- parsely/__init__.py
+++ parsely/__init__.py
@@ -10,3 +10,8 @@ __author__ = "Darius Hall"
     DIR_ERROR,
     FILE_ERROR,
 ) = range(3)
+
+ERRORS = {
+    DIR_ERROR: "config directory error",
+    FILE_ERROR: "config file error",
+}
--- parsely/__main__.py
+++ parsely/__main__.py
@@ -1,12 +1,3 @@
-"""RP To-Do entry point script."""
-# parsely/__main__.py
-
-from parsely import cli, __app_name__
+# parsely/main.py
 
 
-def main():
-    cli.app(prog_name=__app_name__)
-
-
-if __name__ == "__main__":
-    main()
